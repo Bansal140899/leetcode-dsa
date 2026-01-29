92346d35ec1945e5ce66a4a3572033faead122ee
ajay kuamr bansal <146983181+Bansal140899@users.noreply.github.com>
2026-01-29 07:02:27 +0530

    Time: 19 ms (12.23%), Space: 49.7 MB (67.13%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        int sum = 0;
+        Arrays.sort(nums);
+        for(int i = nums.length-1; i>=0;i-=2){
+            sum = sum + Math.min(nums[i] ,nums[i-1]);
+
+        }
+        return sum;
+    }
+}
